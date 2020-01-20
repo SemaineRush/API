bcd1643886c2e2ce3985a5fa742ff9107893517a
sallrouget <sallrouget@gmail.com>
2020-01-20 16:03:31 +0100

    apiresource

--- src/Entity/Elections.php
+++ src/Entity/Elections.php
@@ -4,10 +4,12 @@ namespace App\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ElectionsRepository")
+ * @ApiResource()
  */
 class Elections
 {
--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -5,10 +5,13 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
 use Symfony\Component\Security\Core\User\UserInterface;
+
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
+* @ApiResource()
  */
 class Users implements UserInterface
 {
