c0f07f1042d9062c9944dfd43ae9a60b3fc6bcb6
s4mking <samuel.simonney@golem.ai>
2020-01-22 11:41:19 +0100

    daefgzhtzd

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-die($_ENV);
-
+var_dump($_ENV);
+die();
 require dirname(__DIR__) . '/config/bootstrap.php';
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
