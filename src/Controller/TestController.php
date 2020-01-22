509b14938ec5c3d1a1b4819411eb0c47c20ca522
s4mking <samuel.simonney@golem.ai>
2020-01-22 12:45:51 +0100

    Change

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -9,14 +9,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+
+/**
+ * @Route("/")
+ * 
+ */
 class TestController extends AbstractController
 {
     /**
      * @Route("/", name="message_index", methods={"GET"})
      */
-    public function index()
+    public function index(): Response
     {
         $entityManager = $this->getDoctrine()->getManager();
-        $entityManager->flush();
+        var_dump($entityManager);
+        die;
+        return $this->render('message/new.html.twig', []);
     }
 }
