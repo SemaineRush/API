0cdfa02c9edbd2e6c3e001bcd0ca041e02b0b86c
s4mking <samuel.simonney@golem.ai>
2020-01-22 11:37:25 +0100

    Test heroku

--- public/index.php
+++ public/index.php
@@ -4,8 +4,9 @@ use App\Kernel;
 use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__) . '/config/bootstrap.php';
+die($_ENV);
 
+require dirname(__DIR__) . '/config/bootstrap.php';
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
 
