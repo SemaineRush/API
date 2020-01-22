db5d7fde8b7322791dbfc30711846cf9c4cf68df
s4mking <samuel.simonney@golem.ai>
2020-01-22 17:36:49 +0100

    Remove FosUser

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,13 +2,14 @@
 
 namespace App\Controller;
 
-use App\Repository\CandidateRepository;
+use App\Entity\Election;
 use App\Repository\MessageRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\CandidateRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 
 /**
@@ -22,9 +23,15 @@ class TestController extends AbstractController
      */
     public function index(CandidateRepository $testsam): Response
     {
-
-        $entityManager = $this->getDoctrine()->getManager();
-        var_dump($testsam->findAll());
+        // $sam = new Election;
+        // $d = new \DateTime('2011-01-01T15:03:01.012345Z');
+        // $sam->setEndtest($d);
+        // $sam->setName('toto');
+        // $sam->setLocalisation('toto');
+        // $sam->setStart($d);
+        // $entityManager = $this->getDoctrine()->getManager();
+        // $entityManager->persist($sam);
+        // $entityManager->flush();
         return $this->render('email/base.html.twig', []);
     }
 }
