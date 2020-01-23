0a8e1532c7594a951567fc5f27eca1ba14e4fe79
s4mking <samuel.simonney@golem.ai>
2020-01-23 10:18:25 +0100

    Test

--- src/Controller/ApiAuthController.php
+++ src/Controller/ApiAuthController.php
@@ -62,4 +62,30 @@ class ApiAuthController extends AbstractController
         //     'password' => $data['password']
         // ], 307);
     }
+
+
+    /**
+     * @Route(path="api/auth/reset", methods={"POST"}, name="api_auth_reset")
+     */
+    public function resetPassword(Request $request)
+    {
+        $data = json_decode(
+            $request->getContent(),
+            true
+        );
+        $validator = Validation::createValidator();
+        $constraint = new Assert\Collection(array(
+            // the keys correspond to the keys in the input array
+            'oldpassword' => new Assert\Length(array('min' => 1)),
+            'newpassword' => new Assert\Length(array('min' => 1)),
+            'newpasswordverif' => new Assert\Length(array('min' => 1)),
+        ));
+
+        $violations = $validator->validate($data, $constraint);
+        if ($violations->count() > 0) {
+            return new JsonResponse(["error" => (string) $violations], 500);
+        }
+        $password = $data['newpassword'];
+        return new JsonResponse($password, 200);
+    }
 }
