93e99cd305ba1d299c097d19dd2324dee4269bce
TyomaMtl <scollioudfree.fr@gmail.com>
2020-01-23 10:36:02 +0100

    perform vote controller

--- src/Controller/VoteIncrementationController.php
+++ src/Controller/VoteIncrementationController.php
@@ -3,25 +3,49 @@
 namespace App\Controller;
 
 use App\Repository\CandidateRepository;
+use App\Repository\ElectionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class VoteIncrementationController extends AbstractController {
     
     /**
-     * @Route("/vote/{id}", name="vote", methods={"POST"})
+     * @Route("/vote/{electionId}/{cadidateId}", name="vote", methods={"POST"})
      */
-    public function vote(int $id, CandidateRepository $candidate)
+    public function vote(int $electionId, int $cadidateId, CandidateRepository $candidate, ElectionRepository $election)
     {
         $em = $this->getDoctrine()->getManager();
+        
+        $user = $this->getCurrentUser(); // current user
+        $election = $election->findById($electionId);
+        $voters = $election->getUsers();
 
-        $user = $candidate->findById($id);
-        $user->getNbVotes() + 1;
+        if(in_array($user, $voters)) 
+        {
+            return new $this->json(['status' => '401', 'response' => 'Vous avez déjà voté']);
+        }
 
-        $em->persist($user);
+        $election->addUser($user);
+        $em->persist($election);
+
+        $candidate = $candidate->findById($cadidateId);
+        $candidate->getNbVotes() + 1;
+        $em->persist($candidate);
+        
         $em->flush();
 
         return new $this->json(['status' => '201', 'response' => 'Merci d\'avoir voté']);
+    }
+
+    private function getCurrentUser()
+    {
+        if (null === $token = $this->container->get('security.token_storage')->getToken()) {
+            return;
+        }
+        if (!is_object($user = $token->getUser())) {
+            return;
+        }
+        return $user;
     }
  
 }
