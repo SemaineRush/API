66c80d1ea1df220472ded803ac227a3902e1dd94
s4mking <samuel.simonney@golem.ai>
2020-01-28 22:56:20 +0100

    Local change to bundles Aerios

---
+++ public/assets/js/index.js
@@ -0,0 +1,9 @@
+<script src="https://cdn.onesignal.com/sdks/OneSignalSDK.js" async=""></script>
+<script>
+  var OneSignal = window.OneSignal || [];
+  OneSignal.push(function() {
+    OneSignal.init({
+      appId: "d66405ab-347d-4c80-9ecd-2f5684f0f55b",
+    });
+  });
+</script>
