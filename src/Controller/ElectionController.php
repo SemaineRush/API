52df04c20f8459b007049870f73d9471cb36e24f
s4mking <samuel.simonney@golem.ai>
2020-01-28 17:56:50 +0100

    add idelection

--- src/Controller/ElectionController.php
+++ src/Controller/ElectionController.php
@@ -13,19 +13,18 @@ class ElectionController extends AbstractController
     /**
      * @Route(name="current_election", path="/api/election_current", methods={"GET"})
      */
-    public function electionCurrent(ElectionRepository $elections) : JsonResponse 
+    public function electionCurrent(ElectionRepository $elections): JsonResponse
     {
         $now = new DateTime();
 
         $election = $elections->findAllOrderByEndDesc()[0];
         $lastElection = [];
-        
+
         $candidates = $election->getCandidateElection();
-        
+
         // calcule total score
         $scores = 0;
-        foreach ($candidates as $c)
-        {
+        foreach ($candidates as $c) {
             $score = count($c->getScores());
             $scores += $score;
         }
@@ -37,37 +36,33 @@ class ElectionController extends AbstractController
 
         // candidate score
         $candidatesElection = [];
-        foreach ($candidates as $candidate)
-        {
+        foreach ($candidates as $candidate) {
             $candidateScore = count($candidate->getScores());
 
             $candidatePercentage = null;
-            if ($scores != 0)
-            {
+            if ($scores != 0) {
                 $candidatePercentage = ($candidateScore / $scores) * 100;
             }
 
             $candidatesElection[$candidate->getUserRelated()->getId()] = [
+                "idCandidate" => $candidate->getId(),
                 "votes" => $candidateScore,
                 "percentage" => $candidatePercentage
             ];
         }
-        
+
         $lastElection['id'] = $election->getId();
         $lastElection['name'] = $election->getName();
         $lastElection['total_votes'] = $scores;
         $lastElection['white_votes'] = $scoreWhite;
 
-        if ($now > $election->getEndduration()) 
-        {
+        if ($now > $election->getEndduration()) {
             $lastElection['finished'] = true;
             $lastElection['winner'] = array_keys($candidatesElection, max($candidatesElection))[0];
-        }
-        else
-        {
+        } else {
             $lastElection['finished'] = false;
         }
-        
+
         $lastElection['candidates'] = $candidatesElection;
         $lastElection['start'] = $election->getStart();
         $lastElection['end'] = $election->getEndduration();
