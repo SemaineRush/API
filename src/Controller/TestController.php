c4c9400721b94ada1d7d00275667efe9715fb0fc
s4mking <samuel.simonney@golem.ai>
2020-01-28 22:30:17 +0100

    Test samuel notif

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -27,50 +27,51 @@ class TestController extends AbstractController
      */
     public function index(CandidateRepository $testsam): Response
     {
-        $config = new Config('d66405ab-347d-4c80-9ecd-2f5684f0f55b', 'ZjQzZmRmMzktODNkNS00NDU5LWE5MTUtN2E1Yjg4NjBhODU2', 'MDlmMjJmZDEtMzNlZi00YjNjLTlkNGItMTM3ZmFlNmE2ZjA0');
+        $config = new Config('5ba848ee-3147-4157-a4d9-83cf5d4186b9', 'MDc4MDQ5MWYtNjg2MS00ZTk2LThjM2MtNzUwYjEwZTZjODFi', 'MDlmMjJmZDEtMzNlZi00YjNjLTlkNGItMTM3ZmFlNmE2ZjA0');
+        // $config = new Config('d66405ab-347d-4c80-9ecd-2f5684f0f55b', 'ZjQzZmRmMzktODNkNS00NDU5LWE5MTUtN2E1Yjg4NjBhODU2', 'MDlmMjJmZDEtMzNlZi00YjNjLTlkNGItMTM3ZmFlNmE2ZjA0');
         $httpClient = new Psr18Client();
         $requestFactory = $streamFactory = new Psr17Factory();
 
         $oneSignal = new OneSignal($config, $httpClient, $requestFactory, $streamFactory);
 
-        $myApp = $oneSignal->apps()->getOne('d66405ab-347d-4c80-9ecd-2f5684f0f55b');
-        $oneSignal->apps()->createSegment('d66405ab-347d-4c80-9ecd-2f5684f0f55b', [
-            'name' => 'Test Sam',
-            'filters' => [
-                ['field' => 'session_count', 'relation' => '>', 'value' => 1],
-                ['operator' => 'AND'],
-                ['field' => 'tag', 'relation' => '!=', 'key' => 'tag_key', 'value' => '1'],
-                ['operator' => 'OR'],
-                ['field' => 'last_session', 'relation' => '<', 'value' => '30,'],
-            ],
-        ]);
-        $oneSignal->notifications()->add([
-            'contents' => [
-                'en' => 'Notification message'
-            ],
-            'included_segments' => ['All'],
-            'data' => ['foo' => 'bar'],
-            'isChrome' => true,
-            'send_after' => new \DateTime('1 min'),
-            'filters' => [
-                [
-                    'field' => 'tag',
-                    'key' => 'is_vip',
-                    'relation' => '!=',
-                    'value' => 'true',
-                ],
-                [
-                    'operator' => 'OR',
-                ],
-                [
-                    'field' => 'tag',
-                    'key' => 'is_admin',
-                    'relation' => '=',
-                    'value' => 'true',
-                ],
-            ],
-        ]);
-        var_dump($oneSignal->notifications()->getAll());
+        // $myApp = $oneSignal->apps()->getOne('d66405ab-347d-4c80-9ecd-2f5684f0f55b');
+        // $oneSignal->apps()->createSegment('d66405ab-347d-4c80-9ecd-2f5684f0f55b', [
+        //     'name' => 'Test Sam',
+        //     'filters' => [
+        //         ['field' => 'session_count', 'relation' => '>', 'value' => 1],
+        //         ['operator' => 'AND'],
+        //         ['field' => 'tag', 'relation' => '!=', 'key' => 'tag_key', 'value' => '1'],
+        //         ['operator' => 'OR'],
+        //         ['field' => 'last_session', 'relation' => '<', 'value' => '30,'],
+        //     ],
+        // ]);
+        // $oneSignal->notifications()->add([
+        //     'contents' => [
+        //         'en' => 'Notification message'
+        //     ],
+        //     'included_segments' => ['All'],
+        //     'data' => ['foo' => 'bar'],
+        //     'isChrome' => true,
+        //     'send_after' => new \DateTime('1 min'),
+        //     'filters' => [
+        //         [
+        //             'field' => 'tag',
+        //             'key' => 'is_vip',
+        //             'relation' => '!=',
+        //             'value' => 'true',
+        //         ],
+        //         [
+        //             'operator' => 'OR',
+        //         ],
+        //         [
+        //             'field' => 'tag',
+        //             'key' => 'is_admin',
+        //             'relation' => '=',
+        //             'value' => 'true',
+        //         ],
+        //     ],
+        // ]);
+        // var_dump($oneSignal->notifications()->getAll());
         // die;
         $oneSignal->notifications()->add([
             'contents' => [
@@ -92,72 +93,6 @@ class TestController extends AbstractController
         // $entityManager = $this->getDoctrine()->getManager();
         // $entityManager->persist($sam);
         // $entityManager->flush();
-        function sendMessage()
-        {
-            $content      = array(
-                "en" => 'English Message'
-            );
-            $hashes_array = array();
-            array_push($hashes_array, array(
-                "id" => "like-button",
-                "text" => "Like",
-                "icon" => "http://i.imgur.com/N8SN8ZS.png",
-                "url" => "https://yoursite.com"
-            ));
-            array_push($hashes_array, array(
-                "id" => "like-button-2",
-                "text" => "Like2",
-                "icon" => "http://i.imgur.com/N8SN8ZS.png",
-                "url" => "https://yoursite.com"
-            ));
-            $fields = array(
-                'app_id' => "d66405ab-347d-4c80-9ecd-2f5684f0f55b",
-                'included_segments' => array(
-                    'All'
-                ),
-                'data' => array(
-                    "foo" => "bar"
-                ),
-                'contents' => $content,
-                'web_buttons' => $hashes_array
-            );
-
-            $fields = json_encode($fields);
-            print("\nJSON sent:\n");
-            print($fields);
-
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, "https://onesignal.com/api/v1/notifications");
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                'Content-Type: application/json; charset=utf-8',
-                'Authorization: Basic ZjQzZmRmMzktODNkNS00NDU5LWE5MTUtN2E1Yjg4NjBhODU2'
-            ));
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-            curl_setopt($ch, CURLOPT_HEADER, FALSE);
-            curl_setopt($ch, CURLOPT_POST, TRUE);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-
-            $response = curl_exec($ch);
-            curl_close($ch);
-
-            return $response;
-        }
-
-        $response = sendMessage();
-        $return["allresponses"] = $response;
-        $return = json_encode($return);
-
-        $data = json_decode($response, true);
-        print_r($data);
-        $id = $data['id'];
-        print_r($id);
-
-        print("\n\nJSON received:\n");
-        print($return);
-        print("\n");
-
-
         return $this->render('email/base.html.twig', []);
     }
 
