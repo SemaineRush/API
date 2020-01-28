0d1ec93824ea69853bcfa71685e823d9270360b1
s4mking <samuel.simonney@golem.ai>
2020-01-28 22:52:51 +0100

    Add service

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -9,6 +9,7 @@ use OneSignal\OneSignal;
 use App\Repository\MessageRepository;
 use Nyholm\Psr7\Factory\Psr17Factory;
 use App\Repository\CandidateRepository;
+use App\Service\NotificationSender;
 use Symfony\Component\HttpClient\Psr18Client;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,74 +26,9 @@ class TestController extends AbstractController
     /**
      * @Route("/", name="message_index", methods={"GET"})
      */
-    public function index(CandidateRepository $testsam): Response
+    public function index(CandidateRepository $testsam, NotificationSender $notif): Response
     {
-        $config = new Config('5ba848ee-3147-4157-a4d9-83cf5d4186b9', 'MDc4MDQ5MWYtNjg2MS00ZTk2LThjM2MtNzUwYjEwZTZjODFi', 'MDlmMjJmZDEtMzNlZi00YjNjLTlkNGItMTM3ZmFlNmE2ZjA0');
-        // $config = new Config('d66405ab-347d-4c80-9ecd-2f5684f0f55b', 'ZjQzZmRmMzktODNkNS00NDU5LWE5MTUtN2E1Yjg4NjBhODU2', 'MDlmMjJmZDEtMzNlZi00YjNjLTlkNGItMTM3ZmFlNmE2ZjA0');
-        $httpClient = new Psr18Client();
-        $requestFactory = $streamFactory = new Psr17Factory();
-
-        $oneSignal = new OneSignal($config, $httpClient, $requestFactory, $streamFactory);
-
-        // $myApp = $oneSignal->apps()->getOne('d66405ab-347d-4c80-9ecd-2f5684f0f55b');
-        // $oneSignal->apps()->createSegment('d66405ab-347d-4c80-9ecd-2f5684f0f55b', [
-        //     'name' => 'Test Sam',
-        //     'filters' => [
-        //         ['field' => 'session_count', 'relation' => '>', 'value' => 1],
-        //         ['operator' => 'AND'],
-        //         ['field' => 'tag', 'relation' => '!=', 'key' => 'tag_key', 'value' => '1'],
-        //         ['operator' => 'OR'],
-        //         ['field' => 'last_session', 'relation' => '<', 'value' => '30,'],
-        //     ],
-        // ]);
-        // $oneSignal->notifications()->add([
-        //     'contents' => [
-        //         'en' => 'Notification message'
-        //     ],
-        //     'included_segments' => ['All'],
-        //     'data' => ['foo' => 'bar'],
-        //     'isChrome' => true,
-        //     'send_after' => new \DateTime('1 min'),
-        //     'filters' => [
-        //         [
-        //             'field' => 'tag',
-        //             'key' => 'is_vip',
-        //             'relation' => '!=',
-        //             'value' => 'true',
-        //         ],
-        //         [
-        //             'operator' => 'OR',
-        //         ],
-        //         [
-        //             'field' => 'tag',
-        //             'key' => 'is_admin',
-        //             'relation' => '=',
-        //             'value' => 'true',
-        //         ],
-        //     ],
-        // ]);
-        // var_dump($oneSignal->notifications()->getAll());
-        // die;
-        $oneSignal->notifications()->add([
-            'contents' => [
-                'fr' => "L'élection vient de se terminer , viens consulter les résultats"
-            ],
-            'included_segments' => ['All'],
-            'url' => "https://supvote.herokuapp.com/currentelection",
-            'isAnyWeb' => true,
-            'filters' => [],
-        ]);
-        var_dump($oneSignal);
-        // die;
-        // $sam = new Election;
-        // $d = new \DateTime('2011-01-01T15:03:01.012345Z');
-        // $sam->setEndtest($d);
-        // $sam->setName('toto');
-        // $sam->setLocalisation('toto');
-        // $sam->setStart($d);
-        // $entityManager = $this->getDoctrine()->getManager();
-        // $entityManager->persist($sam);
-        // $entityManager->flush();
+        $notif->sendNotif('toto');
         return $this->render('email/base.html.twig', []);
     }
 
---
+++ src/Service/NotificationSender.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Service;
+
+use OneSignal\Config;
+use OneSignal\OneSignal;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Symfony\Component\HttpClient\Psr18Client;
+
+class NotificationSender
+{
+
+
+
+    private $config;
+
+    public function __construct()
+    {
+        $this->config =  new Config('d66405ab-347d-4c80-9ecd-2f5684f0f55b', 'ZjQzZmRmMzktODNkNS00NDU5LWE5MTUtN2E1Yjg4NjBhODU2', 'MDlmMjJmZDEtMzNlZi00YjNjLTlkNGItMTM3ZmFlNmE2ZjA0');
+        $this->httpClient = new Psr18Client();
+    }
+
+
+    public function sendNotif($message)
+    {
+        $requestFactory = $streamFactory = new Psr17Factory();
+        $oneSignal = new OneSignal($this->config, $this->httpClient, $requestFactory, $streamFactory);
+        $oneSignal->notifications()->add([
+            'contents' => [
+                'en' => $message
+            ],
+            'included_segments' => ['All'],
+            'data' => ['foo' => 'bar'],
+            'isAnyWeb' => true,
+        ]);
+    }
+}
